2ef5c07d884b987d0693f60733a49bd2e8c720dd
Yebroni <A42837@alunos.isel.pt>
2020-10-16 20:31:43 +0100

    teste

---
+++ MSSNSI/src/T1/MyAppHello.java
@@ -0,0 +1,68 @@
+package T1;
+
+import processing.core.PApplet;
+
+public class MyAppHello implements InterfaceProcessingApp{
+	private int raio = 30;
+	
+	float radius = 0, theta = 0;
+	float scrX, scrY;
+	int red = 0, green = 0, blue = 0;
+	
+	public void setup(PApplet p) {
+				//p.fill(255, 0 ,0);
+		p.stroke(255, 0, 0);
+		red = 1;
+	}
+	
+	public void draw(PApplet p, float dt) {
+				//p.circle(p.mouseX, p.mouseY, raio);
+		//framerate = 1/tempo
+				//System.out.println(1./dt);
+		
+		if(theta >= 400)
+		    p.noLoop();
+		    
+		  scrX = theta * p.cos(theta);
+		  scrY = theta * p.sin(theta);
+
+		  theta += 0.25;
+
+		  p.rect(scrX + p.width/2, scrY + p.height/2, 1, 1);   
+		  p.println("X: " + scrX + " Y: " + scrY);
+		
+	}
+	
+	public void mousePressed(PApplet p) {
+				//raio++;
+		if( red == 1 ) {
+			p.stroke(0, 255, 0);
+			green = 1;
+			red = 0;
+			blue = 0;
+		}
+		else if(green == 1) {
+			p.stroke(0, 0, 255);
+			blue = 1;
+			red = 0;
+			green = 0;
+		}
+		
+		else if(blue == 1) {
+			p.stroke(255, 0, 0);
+			red = 1;
+			green = 0;
+			blue = 0;
+		}
+	}
+	
+	public void keyPressed(PApplet p) {
+				//p.fill(0, 255, 0);
+	}
+
+	@Override
+	public void mouseReleased(PApplet p) {
+		// TODO Auto-generated method stub
+		
+	}
+}
---
+++ MSSNSI/src/T1/T1_22.java
@@ -0,0 +1,80 @@
+package T1;
+
+import processing.core.PApplet;
+
+public class T1_22 implements InterfaceProcessingApp{
+	
+	private int backgroundColor = 50;
+	private int logoColor = 200;
+
+	@Override
+	public void setup(PApplet p) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void draw(PApplet p, float dt) {
+		// TODO Auto-generated method stub
+		//por o background preto
+		p.background(backgroundColor);
+		audi( p, (int)p.random(0,800), (int)p.random(0, 600) );
+		p.delay(2000);
+		
+		
+	}
+
+	@Override
+	public void mousePressed(PApplet p) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void keyPressed(PApplet p) {
+		// TODO Auto-generated method stub
+		
+	}
+	
+	
+	
+	public void audi(PApplet p, int oX, int oY) {
+		//por o cinzento da cor do audi
+		p.fill(logoColor);
+		
+		//desenho o circulo no meio do ecra,
+		p.circle(oX, oY, 50);
+		
+		//agora mudo a cor do proximo circulo para dar aquele aspecto tipo anel
+		p.fill(backgroundColor);
+		p.circle(oX, oY, 40);
+		
+		//FAZER TUDO ISTO DE NOVO PARA OS RESTANTES 3 CIRCULOS
+		
+		p.fill(logoColor);
+		p.circle(oX+30, oY, 50);
+		p.fill(backgroundColor);
+		p.circle(oX+30, oY, 40);
+		
+		p.fill(logoColor);
+		p.circle(oX+60, oY, 50);
+		p.fill(backgroundColor);
+		p.circle(oX+60, oY, 40);
+		
+		p.fill(logoColor);
+		p.circle(oX+90, oY, 50);
+		p.fill(backgroundColor);
+		p.circle(oX+90, oY, 40);
+		
+		
+		
+	}
+
+	@Override
+	public void mouseReleased(PApplet p) {
+		// TODO Auto-generated method stub
+		
+	}
+	
+
+}
