93e4086c080e565216591583a0a09a054ed144e3
Pedro Henriques <35275083+eddoasso@users.noreply.github.com>
2020-10-21 16:59:36 +0100

    Criado a class Hello, fornecido nos videos do professor

---
+++ MSSNSI/src/aula/aula1/Hello.java
@@ -0,0 +1,38 @@
+package aula.aula1;
+
+import ProcessingSetup.IProcessingApp;
+import processing.core.PApplet;
+
+public class Hello implements IProcessingApp {
+
+	@Override
+	public void setup(PApplet p) {
+		p.circle(p.width / 2, p.height / 2, 50f);
+	}
+
+	@Override
+	public void draw(PApplet p, float dt) {
+		p.circle(p.mouseX, p.mouseY, 50);
+	}
+
+	public static void main(String[] args) {
+		PApplet.main(Hello.class);
+	}
+
+	@Override
+	public void mousePressed(PApplet p) {
+		// TODO Auto-generated method stub
+	}
+
+	@Override
+	public void keyPressed(PApplet p) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void mouseReleased(PApplet p) {
+		// TODO Auto-generated method stub
+
+	}
+}
